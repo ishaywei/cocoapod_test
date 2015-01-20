bb307f7fb4e48d2aed4ed954ad1b77e4f06c65c4
Ishay Weinstock <ishayweinstock@Ishay-Weinstocks-MacBook-Pro.local>
2015-01-20 14:12:13 +0200

    - Test

--- cocoapods_testTests/cocoapods_testTests.m
+++ cocoapods_testTests/cocoapods_testTests.m
@@ -31,10 +31,10 @@
 }
 
 - (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+//    // This is an example of a performance test case.
+//    [self measureBlock:^{
+//        // Put the code you want to measure the time of here.
+//    }];
 }
 
 @end
